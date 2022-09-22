993cb7cebcfcbfaee78c199049de4c6574effc70
Blake Lockley <bwlockley@gmail.com>
2022-09-22 15:16:39 +1000

    Add disc menu

--- src/gui.c
+++ src/gui.c
@@ -6,14 +6,41 @@
 
 #include "camera.h"
 #include "model.h"
+#include "selection.h"
 
 #define SHOW_DEMO 1
 
 extern camera_t camera;
 extern model_t model;
+extern selection_t selection;
 
 bool show_add_vertex;
 bool show_add_plane;
+bool show_add_disc;
+
+void set_mode_axis(uint8_t axis) {
+    mode_axis = axis;
+
+    switch (mode_axis){
+    case MODE_AXIS_X:
+        vec3_set(selection.control_axis, 1.0f, 0.0f, 0.0f);
+        break;
+    
+    case MODE_AXIS_Y:
+        vec3_set(selection.control_axis, 0.0f, 1.0f, 0.0f);
+        break;
+    
+    case MODE_AXIS_Z:
+        vec3_set(selection.control_axis, 0.0f, 0.0f, 1.0f);
+        break;
+    
+    default:
+        vec3_set(selection.control_axis, 0.0f, 0.0f, 0.0f);
+        break;
+    }
+
+    update_control_axis(&selection);
+}
 
 // GUI
 
@@ -60,6 +87,42 @@ void MainMenuBar() {
             
             if (igMenuItem_Bool("Add Plane", NULL, show_add_plane, true))
                 show_add_plane = !show_add_plane;
+            
+            if (igMenuItem_Bool("Add Disc", NULL, show_add_disc, true))
+                show_add_disc = !show_add_disc;
+            
+            igEndMenu();
+        }
+        
+        if (igBeginMenu("Mode", true))
+        {
+            if (igMenuItem_Bool("Select", NULL, !mode, true))
+                mode = MODE_SELECT;
+            
+            if (igMenuItem_Bool("Translate", NULL, mode & MODE_TRANSLATE, true))
+                mode = MODE_TRANSLATE;
+            
+            if (igMenuItem_Bool("Rotate", NULL, mode & MODE_ROTATE, true))
+                mode = MODE_ROTATE;
+            
+            if (igMenuItem_Bool("Scale", NULL, mode & MODE_SCALE, true))
+                mode = MODE_SCALE;
+
+            igSeparator();
+
+            igMenuItem_Bool("Confine to axis", NULL, false, false);
+
+            if (igMenuItem_Bool("None", NULL, !mode_axis, true))
+                set_mode_axis(MODE_AXIS_NONE);
+            
+            if (igMenuItem_Bool("X Axis", NULL, mode_axis & MODE_AXIS_X, true))
+                set_mode_axis(MODE_AXIS_X);
+            
+            if (igMenuItem_Bool("Y Axis", NULL, mode_axis & MODE_AXIS_Y, true))
+                set_mode_axis(MODE_AXIS_Y);
+            
+            if (igMenuItem_Bool("Z Axis", NULL, mode_axis & MODE_AXIS_Z, true))
+                set_mode_axis(MODE_AXIS_Z);
             
             igEndMenu();
         }
@@ -128,6 +191,57 @@ void AddPlaneMenu() {
     }
 }
 
+void AddDiscMenu() {
+    static vec3 pos = (vec3){ 0.0f, 0.5f, 0.0f};
+    static float radius = 0.5f;
+    static int n_vertices = 12;
+    
+    if (igBegin("Add Disc", &show_add_disc, ImGuiWindowFlags_NoCollapse)) {
+        igText("Position");
+        igInputFloat("Position X", &pos[0], 0.1, 1.0, "%.3f", 0);
+        igInputFloat("Position Y", &pos[1], 0.1, 1.0, "%.3f", 0);
+        igInputFloat("Position Z", &pos[2], 0.1, 1.0, "%.3f", 0);
+        
+        igText("Radius");
+        igInputFloat("Radius", &radius, 0.1, 1.0, "%.3f", 0);
+        
+        igText("Num. Vertices");
+        igInputInt("Num. Vertices", &n_vertices, 1, 2, 0);
+            
+        if (igButton("Add", (struct ImVec2) {0,0})) {
+            float step = (M_PI * 2) / n_vertices;
+
+            // center
+            uint32_t center = add_vertex(&model, pos);
+
+            // edge vertices
+            uint32_t vertices[n_vertices];
+            
+            for (int i = 0; i < n_vertices; i++) {
+                vec3 v;
+                vec3_add(v, pos, (vec3){cosf(step * i) * radius, 0.0f, sinf(step * i) * radius});
+
+                vertices[i] = add_vertex(&model, v);
+            }
+
+            uint32_t indices[3];
+            
+            // face sectors
+            for (int i = 0; i < n_vertices; i++) {
+                indices[0] = center;
+                indices[1] = vertices[i];
+                indices[2] = vertices[(i + 1) % n_vertices];
+                add_face(&model, indices, 3);
+            }
+
+            show_add_disc = false;
+        }
+
+        igEnd();
+    }
+}
+
+
 void DebugWindow() {
     ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration 
         | ImGuiWindowFlags_AlwaysAutoResize
@@ -184,6 +298,9 @@ void gui_update() {
 
     if (show_add_plane)
         AddPlaneMenu();
+
+    if (show_add_disc)
+        AddDiscMenu();
 
     #if SHOW_DEMO
     igShowDemoWindow(NULL);
--- src/gui.h
+++ src/gui.h
@@ -16,4 +16,18 @@ void gui_terminate();
 void gui_update();
 void gui_render();
 
+// Modes
+
+#define MODE_SELECT    0b0000
+#define MODE_TRANSLATE 0b0001
+#define MODE_ROTATE    0b0010
+#define MODE_SCALE     0b0100
+
+#define MODE_AXIS_NONE 0b0000
+#define MODE_AXIS_X    0b0001
+#define MODE_AXIS_Y    0b0010
+#define MODE_AXIS_Z    0b0100
+
+uint8_t mode, mode_axis;
+
 #endif  // GUI_H
