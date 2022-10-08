296feb45dc7a77665a09b123611c3b65dc16623c
Blake Lockley <bwlockley@gmail.com>
2022-10-08 17:33:00 +1100

    Improve icosphere primitive

--- src/primitives.c
+++ src/primitives.c
@@ -73,91 +73,96 @@ void build_icosphere(model_t *model, vec3 pos, float radius, int order) {
     uint32_t n_vertices = 10 * ((uint32_t) powf(4.0f, order)) + 2;
     uint32_t n_faces = n_vertices - 2;
 
-    float r = radius;
-    float t = ((1.0f + sqrtf(5.0f)) / 2.0f) * r;
+    float phi = (1.0f + sqrtf(5.0f)) / 2.0f;
+    float dist = sqrtf(phi * phi + 1.0f);
+
+    float p = (phi / dist) * radius;
+    float q = (1 / dist) * radius;
 
     uint32_t indicies[n_vertices];
-    indicies[0]  = add_vertex(model, (vec3){-r,  t,  0});
-    indicies[1]  = add_vertex(model, (vec3){ r,  t,  0});
-    indicies[2]  = add_vertex(model, (vec3){-r, -t,  0});
-    indicies[3]  = add_vertex(model, (vec3){ r, -t,  0});
+    indicies[0]  = add_vertex(model, (vec3){ q,  p,  0});
+    indicies[1]  = add_vertex(model, (vec3){-q,  p,  0});
 
-    indicies[4]  = add_vertex(model, (vec3){ 0, -r,  t});
-    indicies[5]  = add_vertex(model, (vec3){ 0,  r,  t});
-    indicies[6]  = add_vertex(model, (vec3){ 0, -r, -t});
-    indicies[7]  = add_vertex(model, (vec3){ 0,  r, -t});
+    indicies[2]  = add_vertex(model, (vec3){ 0,  q,  p});
+    indicies[3]  = add_vertex(model, (vec3){ 0,  q, -p});
 
-    indicies[8]  = add_vertex(model, (vec3){ t,  0, -r});
-    indicies[9]  = add_vertex(model, (vec3){ t,  0,  r});
-    indicies[10] = add_vertex(model, (vec3){-t,  0, -r});
-    indicies[11] = add_vertex(model, (vec3){-t,  0,  r});
+    indicies[4]  = add_vertex(model, (vec3){ p,  0,  q});
+    indicies[5]  = add_vertex(model, (vec3){ p,  0, -q});
+    indicies[6]  = add_vertex(model, (vec3){-p,  0, -q});
+    indicies[7]  = add_vertex(model, (vec3){-p,  0,  q});
+    
+    indicies[8]  = add_vertex(model, (vec3){ 0, -q,  p});
+    indicies[9]  = add_vertex(model, (vec3){ 0, -q, -p});
+    
+    indicies[10] = add_vertex(model, (vec3){ q, -p,  0});
+    indicies[11] = add_vertex(model, (vec3){-q, -p,  0});
 
-    face_t *faces[n_faces];
+    uint32_t faces[n_faces];
     uint32_t face_indices[3];
 
-    face_indices[0] = 0; face_indices[1] = 1; face_indices[2] = 5;
+    face_indices[0] = 0; face_indices[1] = 1; face_indices[2] = 2;
     faces[0] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 0; face_indices[1] = 1; face_indices[2] = 7;
+    face_indices[0] = 1; face_indices[1] = 0; face_indices[2] = 3;
     faces[1] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 7; face_indices[1] = 0; face_indices[2] = 10;
+    face_indices[0] = 0; face_indices[1] = 2; face_indices[2] = 4;
     faces[2] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 4; face_indices[1] = 3; face_indices[2] = 2;
+    face_indices[0] = 0; face_indices[1] = 4; face_indices[2] = 5;
     faces[3] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 11; face_indices[1] = 10; face_indices[2] = 0;
+    face_indices[0] = 0; face_indices[1] = 5; face_indices[2] = 3;
     faces[4] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 0; face_indices[1] = 5; face_indices[2] = 11;
+    face_indices[0] = 1; face_indices[1] = 3; face_indices[2] = 6;
     faces[5] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 7; face_indices[1] = 1; face_indices[2] = 8;
+    face_indices[0] = 1; face_indices[1] = 6; face_indices[2] = 7;
     faces[6] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 5; face_indices[1] = 1; face_indices[2] = 9;
+    face_indices[0] = 1; face_indices[1] = 7; face_indices[2] = 2;
     faces[7] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 9; face_indices[1] = 1; face_indices[2] = 8;
+    face_indices[0] = 2; face_indices[1] = 8; face_indices[2] = 4;
     faces[8] = add_face(model, face_indices, 3);
-
-    face_indices[0] = 5; face_indices[1] = 11; face_indices[2] = 4;
+    
+    face_indices[0] = 2; face_indices[1] = 7; face_indices[2] = 8;
     faces[9] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 5; face_indices[1] = 9; face_indices[2] = 4;
+    face_indices[0] = 3; face_indices[1] = 5; face_indices[2] = 9;
     faces[10] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 4; face_indices[1] = 9; face_indices[2] = 3;
+    face_indices[0] = 3; face_indices[1] = 9; face_indices[2] = 6;
     faces[11] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 3; face_indices[1] = 8; face_indices[2] = 9;
+    face_indices[0] = 4; face_indices[1] = 8; face_indices[2] = 10;
     faces[12] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 8; face_indices[1] = 3; face_indices[2] = 6;
+    face_indices[0] = 5; face_indices[1] = 10; face_indices[2] = 9;
     faces[13] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 7; face_indices[1] = 6; face_indices[2] = 8;
+    face_indices[0] = 5; face_indices[1] = 4; face_indices[2] = 10;
     faces[14] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 7; face_indices[1] = 6; face_indices[2] = 10;
+    face_indices[0] = 6; face_indices[1] = 11; face_indices[2] = 7;
     faces[15] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 6; face_indices[1] = 10; face_indices[2] = 2;
+    face_indices[0] = 6; face_indices[1] = 9; face_indices[2] = 11;
     faces[16] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 10; face_indices[1] = 11; face_indices[2] = 2;
+    face_indices[0] = 7; face_indices[1] = 11; face_indices[2] = 8;
     faces[17] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 3; face_indices[1] = 6; face_indices[2] = 2;
+    face_indices[0] = 8; face_indices[1] = 11; face_indices[2] = 10;
     faces[18] = add_face(model, face_indices, 3);
 
-    face_indices[0] = 11; face_indices[1] = 4; face_indices[2] = 2;
+    face_indices[0] = 9; face_indices[1] = 10; face_indices[2] = 11;
     faces[19] = add_face(model, face_indices, 3);
 
     for (int level = 0; level < order; level++) {
         for (int i = 0; i < 20; i++) {
-            face_t *face = faces[i];
+            face_t *face = &model->faces[faces[i]];
 
             uint32_t ia = face->indices[0];
             uint32_t ib = face->indices[1];
@@ -172,28 +177,31 @@ void build_icosphere(model_t *model, vec3 pos, float radius, int order) {
             vec3_add(ab, a, b);
             vec3_scale(ab, ab, 0.5);
             vec3_normalize(ab, ab);
-            vec3_scale(ab, ab, radius * 2);
+            vec3_scale(ab, ab, radius);
             uint32_t iab = add_vertex(model, ab);
             
             vec3_add(ac, a, c);
             vec3_scale(ac, ac, 0.5);
             vec3_normalize(ac, ac);
-            vec3_scale(ac, ac, radius * 2);
+            vec3_scale(ac, ac, radius);
             uint32_t iac = add_vertex(model, ac);
             
             vec3_add(bc, b, c);
             vec3_scale(bc, bc, 0.5);
             vec3_normalize(bc, bc);
-            vec3_scale(bc, bc, radius * 2);
+            vec3_scale(bc, bc, radius);
             uint32_t ibc = add_vertex(model, bc);
 
             add_face_tri(model, ia, iab, iac);
-            add_face_tri(model, ib, iab, ibc);
+            add_face_tri(model, ib, ibc, iab);
             add_face_tri(model, ic, iac, ibc);
 
             face->indices[0] = iab;
-            face->indices[1] = iac;
-            face->indices[2] = ibc;
+            face->indices[1] = ibc;
+            face->indices[2] = iac;
+            
+            calculate_normal(model, face->normal, face->indices, face->len);
+            calculate_midpoint(model, face->midpoint, face->indices, face->len);
         }
     }
 
