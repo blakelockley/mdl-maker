343730336402b5c14cc0f8c2a2c0abdbdb2ead6a
Blake Lockley <bwlockley@gmail.com>
2023-03-18 16:50:48 +1100

    Add building to face plane

--- src/builder.c
+++ src/builder.c
@@ -3,6 +3,7 @@
 #include "model.h"
 #include "utils.h"
 #include "renderers.h"
+#include "picker.h"
 #include "glfw.h"
 
 extern GLFWwindow *window;
@@ -10,10 +11,14 @@ extern GLFWwindow *window;
 extern camera_t camera;
 extern model_t model;
 
+extern picker_t picker;
+
 int normal_axis = 1; // Y
 
 // fill out variables
 vec3 n, p0, l0, l;
+
+bool is_visible = true;
 
 
 void init_builder() {
@@ -36,16 +41,44 @@ void set_building_plane(vec3 origin, vec3 normal) {
 
 void toggle_building_plane() {
     normal_axis = (normal_axis + 1) % 3;
-}
-
-void update_builder() {
-    vec3_copy(p0, camera.origin);
 
     vec3_zero(n);
     n[normal_axis] = 1.0f;
 }
 
+void update_builder() {
+    vec3_copy(p0, camera.origin);
+
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoMove;
+
+    const ImGuiViewport* viewport = igGetMainViewport();
+    ImVec2 work_pos = viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
+    ImVec2 work_size = viewport->WorkSize;
+    ImVec2 window_pos, window_pos_pivot;
+    window_pos.x = work_pos.x + work_size.x - 10.0f;
+    window_pos.y = work_pos.y + 10.0f;
+    window_pos_pivot.x = 1.0f;
+    window_pos_pivot.y = 0.0f;
+    igSetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+    igSetNextWindowBgAlpha(0.35f); // Transparent background
+
+    if (!igBegin("Edit Verticess", NULL, window_flags))
+        return;
+    
+    igText("Building Plane");
+
+    igCheckbox("Visible", &is_visible);
+
+    igInputFloat3("Origin", p0, "%0.2f", 0);
+    igInputFloat3("Normal", n, "%0.2f", 0);
+
+    igEnd();
+}
+
 void render_builder() {
+    if (!is_visible)
+        return;
+    
     glDepthFunc(GL_LESS);
 
     vec4 color = {1.0f, 1.0f, 1.0f, 0.25f};
@@ -55,6 +88,15 @@ void render_builder() {
 }
 
 void build_vertex(float mouse_x, float mouse_y) {
+    uint32_t picked_face_index = render_picker_to_face_id(&picker, &model);
+    if (picked_face_index != INDEX_NOT_FOUND) {
+        
+        face_t *face = &model.faces[picked_face_index];
+        
+        vec3_copy(p0, face->midpoint);
+        vec3_copy(n, face->normal);
+    }
+
     // ray origin l0 is at camera
     vec3_copy(l0, camera.pos);
     
